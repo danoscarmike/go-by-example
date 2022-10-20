c6b710e3362d3fd18ef08579a8ba4bab732a546c
Dan O'Meara <omeara.dan@gmail.com>
2022-10-20 13:01:39 -0700

    moar strings

--- strings.go
+++ strings.go
@@ -29,7 +29,40 @@ func main() {
 	f := func(r rune) bool {
 		return !unicode.IsLetter(r)
 	}
-
 	p("FieldsFunc:", s.FieldsFunc("foo☘️ool", f))
 
+	p("HasPrefix:", s.HasPrefix("fooool", "foooo"))
+	p("HasSuffix:", s.HasSuffix("fooool", "ooool"))
+	p("Index:", s.Index("fooool", "oooo"))
+	p("IndexAny:", s.IndexAny("fooool", "jkl;"))
+	p("IndexFunc:", s.IndexFunc("foo☘️ool", f))
+	p("IndexRune:", s.IndexRune("foo☘️ool", '\u2618'))
+	p("Join:", s.Join([]string{"f", "o", "☘️", "l"}, "_"))
+	p("LastIndex:", s.LastIndex("fooool", "oo"))
+	p("LastIndexAny:", s.LastIndexAny("fooool", "fol"))
+
+	g := func(r rune) rune {
+		switch r {
+		case '\u2618':
+			return 'o'
+		case '\u0020':
+			return '!'
+		}
+		return r
+	}
+	p("Map:", s.Map(g, "fo☘️l "))
+
+	p("Repeat:", s.Repeat("fool ", 3))
+	p("Replace:", s.Replace("fool", "o", "☘️", -1))
+	p("Split:", s.Split("fo-ol", "o"))
+	p("SplitAfter:", s.SplitAfter("fool", "o"))
+	p("SplitN:", s.SplitN("foool", "o", 4))
+	p("Title:", s.Title("fool"))
+	p("ToLower:", s.ToLower("fOoL"))
+	p("ToTitle:", s.ToTitle("fOoL Hardy"))
+	p("ToUpper:", s.ToUpper("fOoL Hardy"))
+	p("Trim:", s.Trim(" fooool-", " -"))
+	p("TrimPrefix:", s.TrimPrefix("prefool", "pre"))
+	p("TrimSuffix:", s.TrimSuffix("foolsufferer", "sufferer"))
+	p("TrimSpace:", s.TrimSpace("   fooooool    "))
 }
