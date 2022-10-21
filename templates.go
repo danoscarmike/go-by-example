25a1a948bd1df25be126937ba2dbda470924d47b
Dan O'Meara <danom@google.com>
2022-10-21 15:46:35 -0700

    adds text templates

--- templates.go
+++ templates.go
@@ -18,10 +18,25 @@ func main() {
 		return template.Must(template.New(name).Parse(t))
 	}
 
-	t2 := Create("t2", "{{.Name}}, {{.DateOfBirth}}")
+	t2 := Create("t2", "{{.Name}}: {{.DateOfBirth.Month}} {{.DateOfBirth.Day}}, {{.DateOfBirth.Year}}\n")
 	data := struct {
 		Name        string
 		DateOfBirth time.Time
-	}{"Dan", time.Date(1980, time.Month(4), 14, 0, 0, 0, 0)}
+	}{"Dan", time.Date(1912, time.April, 14, 0, 0, 0, 0, time.UTC)}
 	t2.Execute(os.Stdout, data)
+
+	type animal struct {
+		Real bool
+		Name string
+	}
+	t3 := Create("t3", "{{if .Real}}{{.Name}} are real, seriously! {{else}}{{.Name}} are a myth, fool!{{end}}\n")
+	quoka := animal{true, "quokas"}
+	unicorn := animal{false, "unicorns"}
+	t3.Execute(os.Stdout, quoka)
+	t3.Execute(os.Stdout, unicorn)
+
+	languages := []string{"C++", "Go", "Java", "Python"}
+	t4 := Create("t4", "{{range .}}{{.}} {{end}}\n")
+	t4.Execute(os.Stdout, languages)
+
 }
